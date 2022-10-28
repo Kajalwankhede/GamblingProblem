f53b992a044a59340d0600d033a01b3d31699fa4
Kajakwankhede <Kajalwankhede@example.com>
2022-10-28 20:31:26 +0530

    Added To Check win or loose that Gamble make  bet so win or loose

--- src/com/glambler/Gambling.java
+++
@@ -1,8 +0,0 @@
-package com.glambler;
-public class Gambling {
-    public static void main(String[] args) {
-        int stakeEveryDay=100;
-        int bet=1;
-        System.out.println("***************** WELCOME TO GAMBLING SIMULATOR *****************");
-    }
-}
--- src/com/glambler/GamblingSimulation.java
+++ src/com/glambler/GamblingSimulation.java
@@ -1,4 +1,7 @@
 package com.glambler;
+
+import java.util.Random;
+
 public class GamblingSimulation {
     static int stake = 100;
     static int bet = 1;
@@ -7,6 +10,17 @@ public class GamblingSimulation {
         System.out.println("\nWelcome to Gambling Simulation Problem");
         System.out.println("\nGambler every day stake is " + stake);
         System.out.println("Bet is " + bet);
+        winOrLooseGame();
+
+    }
+    public static void winOrLooseGame() {
+        Random random = new Random();
+        int bet = random.nextInt(2);
+        if (bet== 1) {
+            System.out.println("\nGambler make $1 Win the bet");
+        } else {
+            System.out.println("\nGambler loose the bet");
+        }
 
     }
 
