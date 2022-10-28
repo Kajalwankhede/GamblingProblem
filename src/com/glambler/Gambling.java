52e44bbfd38452faaa0324acaedf1cae62d07ec6
Kajakwankhede <Kajalwankhede@example.com>
2022-10-28 20:18:08 +0530

    Added game start with stake

---
+++ src/com/glambler/Gambling.java
@@ -0,0 +1,8 @@
+package com.glambler;
+public class Gambling {
+    public static void main(String[] args) {
+        int stakeEveryDay=100;
+        int bet=1;
+        System.out.println("***************** WELCOME TO GAMBLING SIMULATOR *****************");
+    }
+}
---
+++ src/com/glambler/GamblingSimulation.java
@@ -0,0 +1,13 @@
+package com.glambler;
+public class GamblingSimulation {
+    static int stake = 100;
+    static int bet = 1;
+
+    public static void main (String[] args) {
+        System.out.println("\nWelcome to Gambling Simulation Problem");
+        System.out.println("\nGambler every day stake is " + stake);
+        System.out.println("Bet is " + bet);
+
+    }
+
+}
